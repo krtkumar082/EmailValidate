152142ce228336917c83fbce46da330501250975
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-09-28 22:24:23 +0530

    Validate . and TLDs

--- src/com/Email/EmailPattern.java
+++ src/com/Email/EmailPattern.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class EmailPattern {
     public static void main(String[] args) {
-    	Pattern p= Pattern.compile("[@]+[a-z]+");
-		Matcher m=p.matcher("@abc");
+    	Pattern p= Pattern.compile("[.]+[a-z]{2,4}");
+		Matcher m=p.matcher(".net");
 		boolean matchfound=m.matches();
 		if(matchfound) {
 			System.out.println("Valid");
