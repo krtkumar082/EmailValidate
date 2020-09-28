f384f9ff2c80215c0ff51393536877fa9902fd55
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-09-28 21:43:43 +0530

    Welcome

--- src/com/Email/EmailPattern.java
+++ src/com/Email/EmailPattern.java
@@ -5,14 +5,5 @@ import java.util.regex.Pattern;
 
 public class EmailPattern {
     public static void main(String[] args) {
-    	Pattern p= Pattern.compile("[a-z A-Z 0-9]+");
-		Matcher m=p.matcher("1");
-		boolean matchfound=m.matches();
-		if(matchfound) {
-			System.out.println("Valid");
-		}
-		else
-			System.out.println("Invalid");
-
-	}
+    	System.out.println("Welcome to Email Validation");
 }
